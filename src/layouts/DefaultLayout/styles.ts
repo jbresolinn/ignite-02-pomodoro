e8675971cfcbc2b2005bbbcfea0ac64d9c36f50d
julia <juliabresolin2@gmail.com>
2022-08-06 13:21:50 -0300

    style: add min height on default layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const LayoutContainer = styled.div`
   max-width: 74rem;
   height: calc(100vh - 10rem);
+  min-height: 34rem;
   margin: 5rem auto;
   padding: 2.5rem;
 
