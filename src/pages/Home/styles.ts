71fc8cdb475f2b733dca09df2a8243950c541fbd
julia <juliabresolin2@gmail.com>
2022-08-06 13:18:26 -0300

    style: form input styling && add datalist

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -28,6 +28,38 @@ export const FormContainer = styled.div`
   flex-wrap: wrap;
 `
 
+const BaseInput = styled.input`
+  background: transparent;
+  height: 2.5rem;
+  border: 0;
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  font-weight: bold;
+  font-size: 1.125rem;
+  padding: 0 0.5rem;
+  color: ${(props) => props.theme['gray-100']};
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
+
+  &:focus {
+    outline: 0;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+`
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
+`
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
+`
+
 export const CountDownContainer = styled.div`
   font: 700 10rem 'Roboto Mono', monospace;
   line-height: 8rem;
@@ -51,4 +83,28 @@ export const Divider = styled.div`
 
   display: flex;
   justify-content: center;
+`
+
+export const StartCountDownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+  font-weight: bold;
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+  transition: background 0.1s;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['green-300']};
+  }
+
+  &:disabled {
+    opacity: 0.7;
+  }
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,11 +5,7 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-}
-
-:focus {
-  outline: 0;
-  box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
+  outline-color: ${(props) => props.theme['green-500']};
 }
 
 body {
@@ -20,5 +16,13 @@ body {
 
 body, input, textarea, button {
   font: 400 1rem 'Roboto', sans-serif;
+}
+
+a, button {
+  cursor: pointer;
+}
+
+button:disabled {
+  cursor: not-allowed;
 }
 `
