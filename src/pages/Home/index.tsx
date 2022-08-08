e7f1638d881bde2dcccd22874eb34d157be08b10
julia <juliabresolin2@gmail.com>
2022-08-08 20:36:01 -0300

    feat: reset cycle form in submit event

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,7 +24,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -34,6 +34,7 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
@@ -41,8 +42,8 @@ export function Home() {
 
   return (
     <HomeContainer>
-      <form>
-        <FormContainer onSubmit={handleSubmit(handleCreateNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
+        <FormContainer>
           <label htmlFor="task">Vou trabalhar em:</label>
           <TaskInput
             type="text"
